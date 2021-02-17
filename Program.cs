1c78edffde6e351d59457b6b3d32fe093b6a62d7
asma20 <jbeli.asma20@gmail.com>
2021-02-17 16:31:50 +0100

    Créer une liste,Afficher toute la liste.Afficher le 4ème élément de la liste,Afficher l’élément à l’index 4 de la liste,Supprimer Bootstrap de la liste,Modifier Javacrit en Javascript,Ajouter C dans la liste,Afficher la liste

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Partie4Exercice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> langague = new List<String>();
+            langague.Add("HTML");
+            langague.Add("CSS");
+            langague.Add("javascript");
+            langague.Add("jquery");
+            langague.Add("php");
+            langague.Add("boostarp");
+            langague.Add("java");
+            foreach (String l in langague) {
+                Console.WriteLine(l);
+            }
+
+            Console.WriteLine("le 4 éme élement "+langague[3]); 
+            Console.WriteLine("l'élement à l'index 4 " + langague[4]);
+            langague.RemoveAt(5);
+            langague[2] = "javascript";
+
+            langague.Add("c");
+
+            foreach (String l in langague)
+            {
+                Console.WriteLine(l);
+            }
+
+
+
+        }
+    }
+}
